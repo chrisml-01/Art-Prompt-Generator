3a7a8704cd2c2ea43234b51d4f3f9d75e6f48b4b
Christianne Marie Luntok <christianne.luntok1@tafensw.net.au>
2020-09-11 14:57:20 +1000

    have added the main functions

---
+++ assets/vendor.js
@@ -0,0 +1,16 @@
+var colorPallete = document.getElementById('color-palette');
+
+var theme = document.getElementById('word-theme')
+var theme2 = document.getElementById('word-theme2')
+var theme3 = document.getElementById('word-theme3')
+
+var color1 = document.getElementById('color-1');
+var color2 = document.getElementById('color-2');
+var color3 = document.getElementById('color-3');
+
+var c1Text = document.getElementById('color-1-text');
+var c2Text = document.getElementById('color-2-text');
+var c3Text = document.getElementById('color-3-text');
+
+var btnGenerate = document.getElementById('generate-prompt');
+var btnTryAgain = document.getElementById('try-again-color');
